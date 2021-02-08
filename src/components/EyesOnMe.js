8a9bc2078d59b46a4190669afaa22d5858887239
juan_carlos_meza <juancmezat@gmail.com>
2021-02-07 18:57:00 -0800

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,26 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesonMe extends Component {
+
+//   makeColorSwatches = () => (
+//     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
+//       return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+//     })
+//   )
+
+ handleOnFocus = () => {
+     console.log('Good!')
+ }
+
+ handleOnBlur = () => {
+    console.log('Hey! Eyes on me!')
+}
+
+  render() {
+    return (
+      <div >
+          <button onFocus={this.handleOnFocus} onBlur={this.handleOnBlur}></button>
+      </div>
+    )
+  }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,25 @@
-// Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+
+//   makeColorSwatches = () => (
+//     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
+//       return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+//     })
+//   )
+handleKeyUp = () => {
+    console.log('Entering password...')
+} 
+
+handleOnChange = () => {
+    console.log(e.target)
+} 
+
+render() {
+return (
+  <div >
+    <input type="password" onKeyUp={this.handleKeyUp} onChange={this.handleOnChange}/>
+  </div>
+)
+}
+}
